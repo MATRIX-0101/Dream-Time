44c1ae9c3f8efeeeb2597047f29070dd994269c8
Nitin <nitinbhardwaj0500@gmail.com>
2024-04-15 16:01:49 +0530

    sorting buttons added

---
+++ src/components/SortingElements.js
@@ -0,0 +1,49 @@
+import React from 'react'
+
+export default function SortingElements() {
+  return (
+    <div className='flex flex-row flex-wrap'>
+      <button
+        className="bg-[#a6c1ee] text-white px-5 py-2 rounded-full hover:bg-[#87acec]"
+        type="button"
+        onClick=''>
+        Happy
+      </button>
+
+      <button
+        className="bg-[#a6c1ee] text-white px-5 py-2 rounded-full hover:bg-[#87acec]"
+        type="button"
+        onClick=''>
+        Horror
+      </button>
+
+      <button
+        className="bg-[#a6c1ee] text-white px-5 py-2 rounded-full hover:bg-[#87acec]"
+        type="button"
+        onClick=''>
+        Mystery
+      </button>
+
+      <button
+        className="bg-[#a6c1ee] text-white px-5 py-2 rounded-full hover:bg-[#87acec]"
+        type="button"
+        onClick=''>
+        Weird
+      </button>
+
+      <button
+        className="bg-[#a6c1ee] text-white px-5 py-2 rounded-full hover:bg-[#87acec]"
+        type="button"
+        onClick=''>
+        Fantasy
+      </button>
+
+      <button
+        className="bg-[#a6c1ee] text-white px-5 py-2 rounded-full hover:bg-[#87acec]"
+        type="button"
+        onClick=''>
+        Fear
+      </button>
+    </div>
+  )
+}
